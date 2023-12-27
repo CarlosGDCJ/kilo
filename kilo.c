841604a2a9f63229972e129bfe5f08b604327c7b
CarlosGDCJ <32521936+CarlosGDCJ@users.noreply.github.com>
2023-12-27 19:06:41 -0300

    drawing rows

--- kilo.c
+++ kilo.c
@@ -1,31 +1,45 @@
+/** Includes **/
 #include <errno.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <sys/ioctl.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-struct termios original_termios;
+#define CTRL_KEY(a) ((a) & 0x1f)
 
+/** Data **/
+struct config {
+    struct termios original_termios;
+    int screenrows;
+    int screencols;
+};
+
+struct config E;
+
+/** Terminal **/
 void die(const char *e)
 {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[1;1H", 6);
     perror(e);
     exit(1);
 }
 
-void cleanUp(void)
+void disableRawMode(void)
 {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.original_termios) == -1)
         die("tcsetattr");
 }
 
 void enableRawMode(void)
 {
-    if (tcgetattr(STDIN_FILENO, &original_termios) == -1)
+    if (tcgetattr(STDIN_FILENO, &E.original_termios) == -1)
         die("tcgetattr");
 
-    atexit(cleanUp);
-    struct termios raw = original_termios;
+    atexit(disableRawMode);
+    struct termios raw = E.original_termios;
 
     // disabling flags (and (&) with negation)
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
@@ -40,30 +54,86 @@ void enableRawMode(void)
 
 }
 
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        *rows = ws.ws_row;
+        *cols = ws.ws_col;
+        return 0;
+    }
+}
+
+// wait for a keypress and return it
+char waitKeypress()
+{
+    int nread;
+    char c;
+
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if(nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+
+    return c;
+}
+
+/** Input **/
+void editorReadKey()
+{
+    char c = waitKeypress();
+
+    switch (c)
+    {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[1;1H", 6);
+            exit(0);
+            break;
+    }
+}
+
+/** output **/
+void editorDrawRows()
+{
+    int i;
+    for (i = 0; i < E.screenrows; i++)
+    {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+void editorRefreshScreen()
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[1;1H", 6);
+
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[1;1H", 6);
+}
+
+/** Init **/
+void initEditor()
+{
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+        die("getWindowSize");
+
+}
 int main(void)
 {
-    // make read return
     enableRawMode();
-    // read 1 byte from stdin and write to c
+    initEditor();
+
     while(1)
     {
-        char c = '\0';
-        // cygwin sometimes raise eagain when read fails
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-            die("read");
-        // is control character (non-printable)
-        if(iscntrl(c))
-        {
-            printf("%d\r\n", c);
-        }
-        else
-        {
-            // print byte and character
-            printf("%d (%c)\r\n", c, c);
-        }
-
-        if (c == 'q')
-            break;
+        editorRefreshScreen();
+        editorReadKey();
     }
     return 0;
 }
