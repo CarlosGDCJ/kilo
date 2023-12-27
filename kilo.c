ca6d13b2a90161d533401ea6083d5f88d0ba6162
CarlosGDCJ <32521936+CarlosGDCJ@users.noreply.github.com>
2023-12-27 20:25:22 -0300

    append buffer for writes

--- kilo.c
+++ kilo.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <string.h>
 
 #define CTRL_KEY(a) ((a) & 0x1f)
 
@@ -17,6 +18,30 @@ struct config {
 };
 
 struct config E;
+
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0};
+
+void abAppend(struct abuf *ab, char *s, int len)
+{
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL)
+        return;
+    
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+    free(ab->b);
+}
 
 /** Terminal **/
 void die(const char *e)
@@ -131,21 +156,27 @@ void editorReadKey()
 }
 
 /** output **/
-void editorDrawRows()
+void editorDrawRows(struct abuf *ab)
 {
-    int i;
-    for (i = 0; i < E.screenrows; i++)
+    int y;
+    for (y = 0; y < E.screenrows; y++)
     {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        abAppend(ab, "~", 1);
+
+        if (y < E.screenrows - 1)
+            abAppend(ab, "\r\n", 2);
     }
 }
 void editorRefreshScreen()
 {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[1;1H", 6);
+    struct abuf ab = ABUF_INIT;
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H", 3);
 
-    editorDrawRows();
-    write(STDOUT_FILENO, "\x1b[1;1H", 6);
+    editorDrawRows(&ab);
+    abAppend(&ab, "\x1b[H", 3);
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 /** Init **/
